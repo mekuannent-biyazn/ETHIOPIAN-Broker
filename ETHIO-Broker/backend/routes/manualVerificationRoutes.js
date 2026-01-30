94c0eadd6e82c6c41580bc8172f8c10ba4695b68
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-01-30 22:30:56 +0300

    Fix typo in authMiddleware import

--- ETHIO-Broker/backend/routes/manualVerificationRoutes.js
+++ ETHIO-Broker/backend/routes/manualVerificationRoutes.js
@@ -5,7 +5,7 @@ const {
   getVerificationFormData,
   adminVerifyPayment
 } = require("../controllers/manualVerificationController");
-const { protect } = require("../middleware/authMIddleware");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
